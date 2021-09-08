90d950651f522db666ae6ad0b0689ec690465394
mcbarkow <legomatt0811@gmail.com>
2021-09-08 13:57:24 -0400

    Update DraconicWizardry.java

--- src/main/java/net/draconic/wizardry/DraconicWizardry.java
+++ src/main/java/net/draconic/wizardry/DraconicWizardry.java
@@ -1,5 +1,13 @@
 package net.draconic.wizardry;
 
-public class DraconicWizardry {
+import net.fabricmc.api.ModInitializer;
+
+public class DraconicWizardry implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
