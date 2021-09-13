6d9cbec48155ad7d9b0cc348ac9afb781c6cc046
mcbarkow <legomatt0811@gmail.com>
2021-09-13 11:46:19 -0400

    UPDATE
    CODE RUNS BUT DOESNT SPAWN ENTITY

--- src/main/java/net/draconic/wizardry/DraconicWizardryClient.java
+++ src/main/java/net/draconic/wizardry/DraconicWizardryClient.java
@@ -2,12 +2,10 @@ package net.draconic.wizardry;
 
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.impl.client.rendering.EntityRendererRegistryImpl;
-import net.minecraft.entity.EntityType;
-import software.bernie.example.registry.EntityRegistry;
-import net.draconic.wizardry.entities.RedPandaEntity;
+import net.draconic.wizardry.client.renderers.RedPandaRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
+
 
 @Environment(EnvType.CLIENT)
 public class DraconicWizardryClient implements ClientModInitializer {
@@ -15,8 +13,8 @@ public class DraconicWizardryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() 
     {
-        EntityRendererRegistry.INSTANCE.register(DraconicWizardryMain.RED_PANDA, 
-        (ctx, context) -> new RedPandaRenderer(ctx));
+        EntityRendererRegistry.register(DraconicWizardryMain.RED_PANDA, 
+        (ctx) -> new RedPandaRenderer(ctx));
     }
     
 }
--- src/main/java/net/draconic/wizardry/DraconicWizardryMain.java
+++ src/main/java/net/draconic/wizardry/DraconicWizardryMain.java
@@ -2,20 +2,14 @@ package net.draconic.wizardry;
 
 import net.draconic.wizardry.entities.RedPandaEntity;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import software.bernie.example.registry.EntityRegistry;
 import software.bernie.geckolib3.GeckoLib;
 
 public class DraconicWizardryMain implements ModInitializer {
 
-    public static final EntityType<RedPandaEntity> RED_PANDA = Registry.register(
-        Registry.ENTITY_TYPE,
-        new Identifier("draconicwizadrymain", "red_panda"),
-        FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, RedPandaEntity::new).dimensions(EntityDimensions.fixed(0.7F,0.7F)).build());
+    public static final EntityType<RedPandaEntity> RED_PANDA = EntityRegistry.buildEntity(RedPandaEntity::new, RedPandaEntity.class, 0.5F, 0.6F, SpawnGroup.CREATURE);
 
     @Override
     public void onInitialize() {
