47c7d8c86fb4240b7e150233f88972a55b7f3e66
mcbarkow <legomatt0811@gmail.com>
2021-12-06 10:46:50 -0500

    Starting to add in dragon

--- src/main/java/net/draconic/wizardry/entities/DragonEntity.java
+++ src/main/java/net/draconic/wizardry/entities/DragonEntity.java
@@ -1,7 +1,10 @@
 package net.draconic.wizardry.entities;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
@@ -11,9 +14,15 @@ public class DragonEntity extends HostileEntity implements IAnimatable
 {
     AnimationFactory factory = new AnimationFactory(this);
 
-    protected DragonEntity(EntityType<? extends HostileEntity> entityType, World world)
+    public DragonEntity(EntityType<? extends HostileEntity> entityType, World world)
     {
         super(entityType, world);
+        this.ignoreCameraFrustum = true;
+    }
+
+    public static DefaultAttributeContainer.Builder generateDragonSpecs()
+    {
+        return AnimalEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 200.0D);
     }
 
     @Override
