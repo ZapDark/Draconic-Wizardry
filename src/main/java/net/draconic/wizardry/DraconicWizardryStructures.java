c5d2db29594ad7aaa1753530ad00d782f24fb2c4
That1homosapien <57409415+That1homosapien@users.noreply.github.com>
2021-12-08 10:36:18 -0500

    Update DraconicWizardryStructures.java

--- src/main/java/net/draconic/wizardry/DraconicWizardryStructures.java
+++ src/main/java/net/draconic/wizardry/DraconicWizardryStructures.java
@@ -1,2 +1,27 @@
 package net.draconic.wizardry;
-//making what's needed first and fixing as I go
+
+//import net.draconic.wizardry.structures.
+import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.chunk.StructureConfig;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
+
+public class DraconicWizardryStructures
+{
+  //public static StructureFeature<DefaultFeatureConfig> DRAGON_NEST = new DragonNestStructure(DefaultFeatureConfig.CODEC);
+  //public static void setupAndRegisterStructureFeatures()
+  {
+    //FabricStructureBuilder.create(new Identifier(DraconicWizardryMain.MODID, "dragon_nest"), DRAGON_NEXT)
+    //.step(GenerationStep.Feature.SURFACE_STRUCTURES)
+    //.defaultConfig(new StructureConfig(
+          //100, /* average distance apart in chunks between spawn attempts */
+          //10, /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE */
+          //409798521 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */))
+    //.superflatFeature(DRAGON_NEST.configure(FeatureConfig.DEFAULT))
+    //.adjustsSurface()
+    //.register();
+  }
+}
