d144b10f877d7ed83972a30e9075e43dc2bdac9c
That1homosapien <57409415+That1homosapien@users.noreply.github.com>
2021-12-08 10:23:47 -0500

    Create dragon_nest.java

---
+++ src/main/java/net/draconic/wizardry/structures/dragon_nest.java
@@ -0,0 +1,32 @@
+package net.draconic.wizardry.structures;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
+import com.telepathicgrunt.structure_tutorial.StructureTutorialMain;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.structure.MarginedStructureStart;
+import net.minecraft.structure.PoolStructurePiece;
+import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.gen.ChunkRandom;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
+import org.apache.logging.log4j.Level;
+
+import java.util.List;
+
