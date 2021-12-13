4f39f5c6859c32f6ca83d0a8a2f7e4a72c836239
mcbarkow <legomatt0811@gmail.com>
2021-12-13 13:41:42 -0500

    Fixed Dragon Model

--- src/main/java/net/draconic/wizardry/entities/DragonEntity.java
+++ src/main/java/net/draconic/wizardry/entities/DragonEntity.java
@@ -38,7 +38,8 @@ public class DragonEntity extends HostileEntity implements IAnimatable
         return HostileEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 200.0D)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 5.0D)
-                .add(EntityAttributes.GENERIC_FLYING_SPEED,2D);
+                .add(EntityAttributes.GENERIC_FLYING_SPEED,2D)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.6D);
     }
 
     @Override
@@ -47,7 +48,7 @@ public class DragonEntity extends HostileEntity implements IAnimatable
         super.initGoals();
         this.goalSelector.add(0, new WanderAroundGoal(this, 0.5D));
         this.goalSelector.add(1, new LookAroundGoal(this));
-        this.goalSelector.add(4, new AttackGoal(this));
+        this.goalSelector.add(2, new AttackGoal(this));
 
     }
 
@@ -68,17 +69,12 @@ public class DragonEntity extends HostileEntity implements IAnimatable
         return false;
     }
 
-    public int getLimitPerChunk()
-    {
-        return 1;
-    }
-
     @Override
     public boolean canSpawn(WorldView view)
     {
         BlockPos blockUnderEntity = new BlockPos(this.getX(), this.getY()-1, this.getZ());
         BlockPos posEntity = new BlockPos(this.getX(), this.getY(), this.getZ());
-        return view.intersectsEntities(this) && this.world.isNight() && !world.containsFluid(this.getBoundingBox())
+        return view.intersectsEntities(this) && this.world.isDay() && !world.containsFluid(this.getBoundingBox())
                 && this.world.getBlockState(posEntity).getBlock().canMobSpawnInside()
                 && this.world.getBlockState(blockUnderEntity).allowsSpawning(view, blockUnderEntity, EntityRegistry.DRAGON)
                 && DragonCanSpawn;
